43aed2ea7d87a6e2f1054ef0db128cf1a880ad65
Nicholas Wise <nicholasaragih@gmail.com>
2025-06-30 00:43:23 +0700

    feat: add Footer + React Icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const page = () => {
   return (
     <main className="bg-gradient-to-b from-[#3B007D] to-[#280068] min-h-screen w-full flex flex-col items-center h-[2000px]">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Footer />
     </main>
   );
 };
