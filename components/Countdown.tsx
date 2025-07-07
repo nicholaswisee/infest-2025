52d618a1c6ff5af2a050c9b0b804286311212a16
Nathanael <nathanifolia@gmail.com>
2025-07-07 23:37:06 +0700

    feat: initialize countdown component

---
+++ components/Countdown.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { Open_Sans } from 'next/font/google';
+import { CountdownProps } from "@/types";
+
+import React, { useEffect, useState } from 'react';
+
+const openSans = Open_Sans({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+const Countdown : React.FC<CountdownProps> = ({targetDate, onFinish}) => {
+    const calculateTimeLeft = (): number => {
+        const now = new Date().getTime();
+        const target = targetDate.getTime();
+        const diff = Math.max(0, Math.floor((target-now)/1000));
+        return diff;
+    };
+ 
+    const [timeLeft, setTimeLeft] = useState<number>(calculateTimeLeft());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeLeft((prev) => {
+                if (prev <= 1) {
+                    clearInterval(interval);
+                    onFinish?.();
+                    return 0;
+                }
+                return calculateTimeLeft();
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [targetDate, onFinish]);
+
+    const hours = Math.floor(timeLeft / 3600)
+        .toString()
+        .padStart(2, "0");
+    const minutes = Math.floor((timeLeft % 3600) / 60)
+        .toString()
+        .padStart(2, "0");
+    const seconds = (timeLeft % 60).toString().padStart(2, "0");
+    return (
+        <div className={`${openSans.className} flex justify-center gap-12 text-[69.86px] leading-[35px] font-normal tracking-[1%] text-center text-white`}>
+            <div className="text-center">
+                <p>{hours}</p>
+                <span className="text-xl text-white">Hours</span>
+            </div>
+            <div className="text-center">
+                <p>{minutes}</p>
+                <span className="text-xl text-white">Minutes</span>
+            </div>
+            <div className="text-center">
+                <p>{seconds}</p>
+                <span className="text-xl text-white">Seconds</span>
+            </div>
+        </div>
+    );
+};
+
+export default Countdown;
