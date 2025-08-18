fb52bc8c097df53a45b71e85bd8021e7e16d6d1b
Nicholas Wise <nicholasaragih@gmail.com>
2025-08-18 18:08:07 +0700

    fix: add suspense wrapper

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import LoginForm from '../../components/LoginForm'
 
 export default function Page() {
   return (
     <main className="relative h-screen w-full isolate overflow-hidden">
+      <Suspense fallback={<div>Loading...</div>}>
         <LoginForm />
+      </Suspense>
     </main>
   )
 }
